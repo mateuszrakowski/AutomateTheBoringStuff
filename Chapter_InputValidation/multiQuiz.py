01034549ccfc870b9b91efa53ddd83c435e8d595
Mateusz Rakowski <mateuszrakowski97@gmail.com>
2023-03-28 18:16:57 +0200

    Multiplication Quiz with working timer, number of tries and output

--- Chapter_InputValidation/multiQuiz.py
+++ Chapter_InputValidation/multiQuiz.py
@@ -6,24 +6,44 @@ def quiz():
     correctAnswer = 0
 
     for questionNumber in range(numberOfQuestions):
+        # Pick two random numbers
         num1 = random.randint(0, 9)
         num2 = random.randint(0, 9)
 
+        # Quiz mathematic question
         prompt = f"#{questionNumber + 1}: {num1} * {num2} = "
 
-        for i in range(3):
+        # Save time to variable every question asked
+        timeout = time.perf_counter()
+
+        for i in range(4):
+            # Handles number of tries with limit to 3
+            if i == 3:
+                print("Out of tries!")
+                break
+
             try:
                 answer = int(input(prompt))
-    
+
+                # Right answer
                 if answer == num1 * num2:
                     correctAnswer += 1
                     print("Correct!")
                     break
+                # Wrong answer
                 print("Incorrect!")
-            
+
+            # Wrong input, eg. string
             except ValueError:
                 print("Incorrect!")
-        
-        print("Out of time!")
-    print("Out of tries!")  
 
+            # Check how many time passed, break if more than 8 seconds
+            if (time.perf_counter() - timeout) > 8:
+                print("Out of time!")
+                break
+
+        time.sleep(1)
+    print(f"Score: {correctAnswer} / {numberOfQuestions}")
+
+
+quiz()
