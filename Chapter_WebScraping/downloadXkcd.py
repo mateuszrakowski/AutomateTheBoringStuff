9ddb982d7e82808c409c3aaee285c8e2e0bfa6b0
Mateusz Rakowski <mateuszrakowski97@gmail.com>
2023-04-08 18:29:39 +0200

    Added Web scraping projects

---
+++ Chapter_WebScraping/downloadXkcd.py
@@ -0,0 +1,40 @@
+# downloadXkcd.py - Downloads every single XKCD comic.
+
+import requests, os, bs4
+
+
+# Starting URL
+url = "https://xkcd.com"
+
+# Store comics in ./xkcd
+os.makedirs("xkcd", exist_ok=True)
+
+while not url.endswith("#"):
+    # Download the page
+    print(f"Downloading page {url}...")
+    res = requests.get(url)
+    res.raise_for_status()
+
+    soup = bs4.BeautifulSoup(res.text, "html.parser")
+
+    # Find the URL of the comic image
+    comicElem = soup.select("#comic img")
+    if comicElem == []:
+        print("Could not find comic image.")
+    else:
+        comicUrl = "https" + comicElem[0]
+        # Download the image
+        print(f"Downloading image {comicUrl}...")
+        res = requests.get(comicUrl)
+        res.raise_for_status()
+    
+    # Save the image to ./xkcd
+    imageFile = open(os.path.join("xkcd", os.path.basename(comicUrl)), "wb")
+
+    for chunk in res.iter_content(100000):
+        imageFile.write(chunk)
+    imageFile.close()
+
+    # Get the Prev button's URL
+    prevLink = soup.select('a[rel="prev"]')[0]
+    url = "https://xkcd.com" + prevLink.get("href")
---
+++ Chapter_WebScraping/mapIt.py
@@ -0,0 +1,13 @@
+#mapIt.py - Launches a map in the browser using an adress from the command line or clipboard.
+
+import webbrowser, sys, pyperclip
+
+
+if len(sys.argv) > 1:
+    # Get address from command line
+    address = ' '.join(sys.argv[1:])
+else:
+    # Get adress from clipboard
+    address = pyperclip.paste()
+
+webbrowser.open('https://www.google.com/maps/place/' + address)
